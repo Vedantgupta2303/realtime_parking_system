eda8bdac9279299e0bcd259b03d45e2e679222d8
Vedantgupta2303 <51325847+Vedantgupta2303@users.noreply.github.com>
2019-10-19 23:43:35 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,10 @@
+# Think to manipulate this using GUI
+from GuiForFrame import counter,counter_2
+frame1 = counter.get()
+frame2 = counter_2.get()
+print(counter.get())
+print(counter_2.get())
+
+# Define the region of interest polygon here
+# Improvement:
+ip = ""
